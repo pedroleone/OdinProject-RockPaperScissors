165d06afd33bff6fc8d75f82634578733e339069
Pedro Leone <pedroleone@gmail.com>
2022-05-30 19:06:07 -0300

    Initial UI for the RPS game

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 function generateRandomNumber() {
-    // Math.floor => retorna o maior inteiro menor ou igual ao numero
-    // Math.floor(5.95) => 5
-    // Math.floor(5.05) => 5
-    // Com números negativos fica estranho
-    // Math.floor(-5.05) => 6
-    
-    // Como o Math.random() retorna num numero entre 0 e 1, 
-    // para obter um numero entre 1 e 3 deve-se multiplicar por 3 e em 
-    // seguida somar com 1
     return Math.floor(Math.random() * 3 + 1)
 }
 
@@ -23,14 +14,7 @@ function computerPlay() {
 }
 
 function playerSelection() {
-    while (true) {
-        let choice = prompt("Choose paper, rock or scissors").toUpperCase();
-        if (choice == 'ROCK' || choice == 'PAPER' || choice == 'SCISSORS') {
-            return choice;
-        } else {
-            alert("Please choose a valid choice!");
-        }
-    }
+
 }
 
 let playerWins = 0;
@@ -54,3 +38,15 @@ for (let i = 0; i < 5; i++) {
 }
 
 console.log(`You won ${playerWins} times`);
+
+const optionButtons = document.querySelectorAll("div.icons img");
+optionButtons.forEach(e => {
+    e.addEventListener('click', function(e) {
+        const selection = e.target.getAttribute("data-option")
+        console.log(selection);
+    })
+})
+
+
+
+
