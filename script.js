5285064a56a5e07b4e94dbd25b2e795cda0c827a
Pedro Leone <pedroleone@gmail.com>
2022-06-02 19:29:38 -0300

    Finished game logic, crude UI

--- script.js
+++ script.js
@@ -14,39 +14,82 @@ function computerPlay() {
 }
 
 let playerWins = 0;
+let numPlays = 0;
+let computerWins = 0;
 
 function decideWinner(playerSelection , computerSelection) {
-    let youWin = false;
+    numPlays++;
+    let result = "You lose!";
 
     
     if (playerSelection == computerSelection) {
-        return "That's a tie!"
+        result = "That's a tie!"
+    } else {
+        if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+            (playerSelection == 'scissors' && computerSelection == 'paper') || 
+            (playerSelection == 'paper' && computerSelection == 'rock')) {
+                playerWins++;
+                result = "You won!"
+            } else {
+                computerWins++;
+                result = "You lost!"
+            }
     }
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper') || 
-        (playerSelection == 'paper' && computerSelection == 'rock')) {
-            playerWins++;
-            youWin = true;
-        }
     
-    paintResult(playerSelection, computerSelection, playerWins);
+    paintResult(playerSelection, computerSelection, result);
 }
 
-function paintResult(playerSelection, computerSelection, playerWins) {
-    const container = document.querySelector("#result")
+function paintResult(playerSelection, computerSelection, result) {
+    const container = document.querySelector("#result");
     const playResult = document.createElement("p");
-    const resultString = playerWins ? "You win!" : "You lose!";
-    playResult.innerHTML = `You: <img src="./assets/${playerSelection}.svg"> - Computer: <img src="./assets/${computerSelection}.svg" class="flipped"> - ${resultString}`;
-    container.appendChild(playResult)
+    playResult.innerHTML = `You: <img src="./assets/${playerSelection}.svg"> - Computer: <img src="./assets/${computerSelection}.svg" class="flipped"> - ${result}`;
+    container.appendChild(playResult);
+    
+}
+
+function endGame() {
+    const annoucement = document.createElement("p");
+    let annoucementeMessage;
+    if (playerWins === computerWins) {
+        annoucementeMessage = "That's a tie! Incredible!"
+    }  
+    if (playerWins > 2) {
+        annoucementeMessage = "You won!"  
+    } else {
+        annoucementeMessage = "You lost!"
+    }
+
+    annoucement.innerHTML = `Game finished! ${annoucementeMessage} Select an option to begin again`;
+
+    const container = document.querySelector("#result");
+    container.appendChild(annoucement);
+
+}
+
+function resetGame() {
+    numPlays = 0;
+    playerWins = 0;
+    computerWins = 0;
+    document.querySelector("#result").innerHTML = "";
+
+
 }
 
 console.log(`You won ${playerWins} times`);
 
-const optionButtons = document.querySelectorAll("div.icons img");
+const optionButtons = document.querySelectorAll("div.icons button");
 optionButtons.forEach(e => {
     e.addEventListener('click', function(e) {
-        const selection = e.target.getAttribute("data-option")
+        if (numPlays == 5 ) {
+            resetGame();
+        }
+        const selection = this.getAttribute("data-option")
         decideWinner(selection, computerPlay());
+        if (numPlays === 5) {
+            endGame();
+        }
+
+        
     })
 })
 
